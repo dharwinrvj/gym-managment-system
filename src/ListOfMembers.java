ebd137c1836ea00ed9ac4a15f2f322bd1755ba22
Dharwin J Ruppa <59426893+DharwinJRuppa@users.noreply.github.com>
2020-11-17 09:45:49 +0530

    gym

--- src/ListOfMembers.java
+++ src/ListOfMembers.java
@@ -23,7 +23,7 @@ public class ListOfMembers extends javax.swing.JFrame {
         {
             Connection con=ConnectionProvider.getCon();
             Statement st=con.createStatement();
-            ResultSet rs=st.executeQuery("select *from member");
+            ResultSet rs=st.executeQuery("select * from member");
             while(rs.next())
             {
                 model.addRow(new Object[]{rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),rs.getString(8),rs.getString(9),rs.getString(10),rs.getString(11)}); 
